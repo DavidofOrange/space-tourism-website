77217236e93e33c0da411d053d36e1fa03cd3eec
David Childerhose <davidoforange@outlook.com>
2021-12-08 16:58:15 +0900

    Added Nav bar component and created CSS utility classes

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <div>
+          <nav>
+            <p>00 Home</p>
+            01 Destination
+            02 Crew
+            03 Technology
+          </nav>
+        </div>
+    );
+};
+
+export default Nav;

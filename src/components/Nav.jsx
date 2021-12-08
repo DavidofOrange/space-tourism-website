4d65c94f262b34e75049d9498de6264489d1a054
David Childerhose <davidoforange@outlook.com>
2021-12-08 21:35:43 +0900

    added typography settings in css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import "./CSS/Nav.css";
 
 function Nav() {
     return (
-        <div className="nav-bar container flex">
+        <div className="nav-bar container flex font-white">
             <div className="nav-icon">00 Home</div>
             <div className="nav-icon">01 Destination</div>
             <div className="nav-icon">02 Crew</div>

d935e538eec8c0eedb1dd556f595f8212d58402d
David Childerhose <davidoforange@outlook.com>
2022-01-01 17:02:30 +0900

    fixed button hover and after effects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,23 +7,22 @@ export default function Home() {
     return (
         <section className='home'>
         <Nav />
-          <div className='container'>
-            <article className='home-text font-white'>
+          <div className='container font-white'>
+            <div>
               <h1 className='text-accent fs-500 ff-sans-cond uppercase letter-spacing-1'>
                 So, you want to travel to
-              </h1>
-              <h1><span className='fs-900 ff-serif '>Space</span></h1>
+              <span className='fs-900 ff-serif'>Space</span></h1>
               <p className='fs-400'>
                  Let’s face it; if you want to go to space, you might as well genuinely go to 
                  outer space and not hover kind of on the edge of it. Well sit back, and relax 
                  because we’ll give you a truly out of this world experience!
               </p>
-            </article>
-            <article>
+            </div>
+            <div>
                 <Link className='large-button uppercase ff-serif fs-600 text-dark bg-white' to='/destination'>
                   <h1 className=''>Explore</h1>
                 </Link>
-            </article>
+            </div>
           </div>
         </section>
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,13 +14,13 @@ function Nav() {
         </div>
         <nav className='flex'>
           <ul className='primary-navigation underline-indicators flex'>
-            <li className='active'><Link className='ff-sans-cond uppercase font-white letter-spacing-2' 
+            <li className='active'><Link className='ff-sans-cond uppercase text-white letter-spacing-2' 
             to='/'><span>00 Home</span></Link></li>
-            <li><Link className='ff-sans-cond uppercase font-white letter-spacing-2' 
+            <li><Link className='ff-sans-cond uppercase text-white letter-spacing-2' 
             to='/destination'><span>01 Destination</span></Link></li>
-            <li><Link className='ff-sans-cond uppercase font-white letter-spacing-2' 
+            <li><Link className='ff-sans-cond uppercase text-white letter-spacing-2' 
             to='/crew'><span>02 Crew</span></Link></li>
-            <li><Link className='ff-sans-cond uppercase font-white letter-spacing-2' 
+            <li><Link className='ff-sans-cond uppercase text-white letter-spacing-2' 
             to='/technology'><span>03 Technology</span></Link></li>
           </ul>
         </nav>

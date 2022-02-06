25736f1fd45204d09159c1e0acc29b11e1aa1c1e
David Childerhose <davidoforange@outlook.com>
2022-02-06 20:34:50 +0900

    Completed crew pages

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -12,7 +12,7 @@ export default function Crew() {
         <>
         <section className="crew">
           <Nav />
-          <main className="grid-container grid-container--crew flow">
+          <main className="grid-container grid-container--crew">
             <h1 className="numbered-title"><span>02</span> Meet your crew</h1>
             <article className="crew-info ff-serif uppercase flow">
               <h2 className="fs-600 flow">{role}</h2>
